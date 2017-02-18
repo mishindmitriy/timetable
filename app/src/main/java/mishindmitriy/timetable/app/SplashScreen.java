ed6a935ceed2c4ae132c5674d3b933734d4a2e7b
Dmitry Mishin <mishin.dmitriy@gmail.com>
2017-02-18 10:43:26 +0400

    fix crash when subject count ==0

--- app/src/main/java/mishindmitriy/timetable/app/SplashScreen.java
+++ app/src/main/java/mishindmitriy/timetable/app/SplashScreen.java
@@ -7,9 +7,11 @@ import android.support.v7.app.AppCompatActivity;
 
 import javax.inject.Inject;
 
+import io.realm.Realm;
 import mishindmitriy.timetable.R;
 import mishindmitriy.timetable.app.schedulesubjects.ScheduleSubjectsActivity;
 import mishindmitriy.timetable.app.shedule.ScheduleActivity;
+import mishindmitriy.timetable.model.ScheduleSubject;
 import mishindmitriy.timetable.utils.Prefs;
 
 /**
@@ -19,13 +21,15 @@ import mishindmitriy.timetable.utils.Prefs;
 public class SplashScreen extends AppCompatActivity {
     @Inject
     protected Prefs prefs;
+    @Inject
+    protected Realm realm;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         TimeTableApp.component().inject(this);
         try {
-            if (prefs.getSelectedThingId() > 0) {
+            if (prefs.getSelectedThingId() > 0 || realm.where(ScheduleSubject.class).count() == 0) {
                 startActivity(new Intent(this, ScheduleActivity.class));
             } else {
                 startActivity(new Intent(this, ScheduleSubjectsActivity.class));
